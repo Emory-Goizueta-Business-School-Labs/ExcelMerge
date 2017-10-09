0927eb27116de927ef26b7bd23f092778d929ef2
Jamie Harrell <jamie.harrell@emory.edu>
2017-10-09 14:35:11 -0400

    Initial commit -- not fully tested

---
+++ excelMerge/MergeExcel.cs
@@ -0,0 +1,119 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+
+namespace excelMerge
+{
+
+    public class MergeExcel
+    {
+        // The contents of this class is highly modified and thus should be considered "inspired" by the original work, which is licensed 
+        // under the CPOL v1.02 license (https://www.codeproject.com/info/cpol10.aspx)
+        //
+        // Original Author: Calabash Sisters
+        // Original Source: https://www.codeproject.com/Tips/715976/Solutions-to-Merge-Multiple-Excel-Worksheets-int
+        //
+        // TODO: Make sure to avoid the double-dot issue
+
+        static string[] excelColumns =
+        { "", // setup for 1-based indexing just for simplicity
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+        "AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ",
+        "BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ",
+        "CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","CQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ",
+        "" // If you have more than 36 x 4 columns in your spreadsheet we're just gonna assume something broke somewhere else. 
+        };
+
+
+        Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+        Excel.Workbook bookDest = null;
+        Excel.Worksheet sheetDest = null;
+
+        //
+        Excel.Workbook bookSource = null;
+        Excel.Worksheet sheetSource = null;
+        string[] _sourceFiles = null;
+        string _destFile = string.Empty;
+        //string _columnEnd = string.Empty;
+        int _headerRowCount = 0;
+        int _currentRowCount = 0;
+        //int _columnCount = 0;
+
+        public MergeExcel(string[] sourcefiles, string destFile, /* string columnEnd,*/ int headerRowCount)
+        {
+            bookDest = (Excel.Workbook)app.Workbooks.Add(Missing.Value);
+            sheetDest = bookDest.Worksheets.Add(Missing.Value, Missing.Value, Missing.Value, Missing.Value) as Excel.Worksheet;
+            sheetDest.Name = "data";
+            _sourceFiles = sourcefiles;
+            _destFile = destFile;
+            //_columnEnd = columnEnd;
+            _headerRowCount = headerRowCount;
+        }
+
+        // open worksheet
+        void OpenBook(string fileName)
+        {
+            bookSource = app.Workbooks._Open(fileName, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            sheetSource = bookSource.Worksheets[1] as Excel.Worksheet;
+        }
+
+        void CloseBook()
+        {
+            bookSource.Close(false, Missing.Value, Missing.Value);
+        }
+
+        void CopyHeader()
+        {
+            string columnEnd = excelColumns[sheetSource.UsedRange.Columns.Count];
+            Excel.Range range = sheetSource.get_Range("A1", columnEnd + _headerRowCount.ToString());
+            range.Copy(sheetDest.get_Range("A1", Missing.Value));
+            _currentRowCount += _headerRowCount;
+        }
+
+        void CopyData()
+        {
+            int sheetRowCount = sheetSource.UsedRange.Rows.Count;
+            //int sheetColumnCount = sheetSource.UsedRange.Columns.Count;
+            string columnEnd = excelColumns[sheetSource.UsedRange.Columns.Count];
+
+            Excel.Range range = sheetSource.get_Range(string.Format("A{0}", _headerRowCount), columnEnd + sheetRowCount.ToString());
+            range.Copy(sheetDest.get_Range(string.Format("A{0}", _currentRowCount), Missing.Value));
+            _currentRowCount += range.Rows.Count;
+        }
+
+        void Save()
+        {
+            bookDest.Saved = true;
+            bookDest.SaveCopyAs(_destFile);
+        }
+
+        void Quit()
+        {
+            app.Quit();
+        }
+
+        void DoMerge()
+        {
+            bool b = false;
+            foreach (string strFile in _sourceFiles)
+            {
+                OpenBook(strFile);
+                if (b == false)
+                {
+                    CopyHeader();
+                    b = true;
+                }
+                CopyData();
+                CloseBook();
+            }
+            Save();
+            Quit();
+        }
+
+        public static void DoMerge(string[] sourceFiles, string destFile, /*string columnEnd,*/ int headerRowCount)
+        {
+            new MergeExcel(sourceFiles, destFile, /* columnEnd, */ headerRowCount).DoMerge();
+        }
+
+    }
+}
---
+++ excelMerge/Program.cs
@@ -0,0 +1,158 @@
+﻿/*
+ * Program: excelMerge.exe
+ * Author: Jamie Anne Harrell (jamie.harrell@emory.edu)
+ * Date: 10/14/2016
+ * 
+ * 
+ * NAME
+ * excelMerge.exe - command line utility to merge all excel documents within a directory into a single excel workbook.
+ * 
+ * SYNOPSIS
+ * excelMerge.exe [OPTION] [source_directory]
+ * 
+ * DESCRIPTION
+ * excelMerge.exe has a very specific use case - to aggregate data from multiple Excel spreadsheets with the exact same structure into a single
+ * final spreadsheet. A good use, for example, would be to combine multiple sources of identically structured spreadsheets that span different
+ * time periods into a single spreadsheet in order to reduce the number of ETL jobs used to load the data into a database. The output excel
+ * file will be placed in the directory above the named directory, and the output file name will be the name of the directory with the .xlsx
+ * suffix added. For example, a folder named c:\path\to\TEST that contains 5 spreadsheets with identical structures will produce a file named
+ * c:\path\to\TEST.xlsx
+ * 
+ * You may specify in a single optional parameter the number of the spreadsheet header row. During concatenation of the data from the various
+ * spreadsheets, that row number will be assumed to be a header, and all rows before it will be skipped. The header row will appear only once
+ * in the output file. Only the first worksheet in an excel workbook is processed. Non-excel files (not xls or xlsx) in the direcectory are
+ * ignored.
+ * 
+ * *** Warning: no worksheet structural validation is present. If your header rows do not match, the files will still be processed as-is and
+ * *** data from the unmatching columns appended as-is to the final output. Output file is overwritten WITHOUT NOTICE.
+ * 
+ * * TODO: MAKE THIS MORE FLEXIBLE AND ADD HEADER VALIDATION
+ * 
+ * GENERAL
+ * Only a single operational mode is supported with one optional parameter as noted below.
+ * 
+ * excelMerge.exe [PARAMETER] [source_directory]
+ * 
+ * PARAMETERS
+ *  -h=N, --header=N, /v=N, /header=N   where N is the integer row number where the header is in the spreadsheet. Rows prior to this are skipped.
+ *  
+ * EXAMPLES:
+ *  
+ *  excelMerge.exe c:\path\to\folder
+ *  
+ *  excelMerge.exe -h=2 c:\path\to\folder
+ *  
+ *  
+ * CREDITS: Various contributions and ideas for this project were borrowed from:
+ *  Calabash Sisters        https://www.codeproject.com/Tips/715976/Solutions-to-Merge-Multiple-Excel-Worksheets-int (Solution 1 - CPOL license v1.02)
+ *  Dmitry Martovoi         https://stackoverflow.com/questions/17367411/cannot-close-excel-exe-after-interop-process/17367570#17367570
+ *  MSDN, of Course         https://msdn.microsoft.com/en-us/library/microsoft.office.interop.excel.workbooks.open.aspx
+ * 
+ * 
+ * License Notes: the class MergeExcel.cs is licensed under the Open Source CPOL v1.02 license https://www.codeproject.com/info/cpol10.aspx
+ *
+ * All other files:
+ *  
+ * License: MIT / Open Source
+ * 
+ * Copyright (c) 2017 Emory Goizueta Business School
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ * 
+ */
+
+using System;
+using System.Linq;
+using System.IO;
+
+namespace excelMerge
+{
+
+
+    class Program
+    {
+
+        public static int headerRow = 1;
+
+        static void Usage()
+        {
+            Console.WriteLine("EXCELMERGE USAGE:");
+            Console.WriteLine("========================================");
+            Console.WriteLine("excelMerge.exe [PARAMETER] [source_directory]");
+            Console.WriteLine(" PARAMETER:");
+            Console.WriteLine(" -h=N, --header=N, /v=N, /header=N");
+            Console.WriteLine(" EXAMPLES:");
+            Console.WriteLine(@" excelMerge.exe c:\path\to\folder");
+            Console.WriteLine(@" excelMerge.exe -h=2 c:\path\to\folder");
+        }
+
+        static int Main(string[] args)
+        {
+            int headerRow = 1;
+            string headerRowOption = "";
+            String directory = "";
+            switch (args.Length) {
+                case 1:
+                    directory = args[0];
+                    break;
+                case 2:
+                    headerRowOption = args[0];
+                    directory = args[1];
+                    char[] separator = { '=' }; 
+                    string[] tokens = headerRowOption.Split(separator, 2);
+                    if (tokens.Count() != 2) { Usage(); return 1; }
+                    if ((tokens[0] != "-h") && (tokens[0] != "--header") && (tokens[0] != "/h") && (tokens[0] != "/header") ){ Usage(); return 2; }
+                    if (!int.TryParse(tokens[1],out headerRow)) { Usage(); return 3; }
+                    break;
+                default:
+                    Usage();
+                    return (1);
+            }
+               
+            // make directory relative if not found absolutely
+            if (!Directory.Exists(directory))
+            {
+                directory = System.IO.Directory.GetCurrentDirectory() + @"\" + directory;
+                if (!Directory.Exists(directory))
+                {
+                   System.Console.WriteLine("Invalid directory");
+                   return (4);
+                }
+            }
+
+            // Get list of all files in the directory
+            string[] fileEntries = Directory.GetFiles(directory);
+            string destination = directory + ".xlsx";
+            System.Console.WriteLine("Processing directory " + directory + " into " + destination);
+            try
+            {
+                MergeExcel.DoMerge(fileEntries, destination, headerRow);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error processing files: '{0}'", e.Message);
+                return (-1);
+            }
+            
+            return (0);
+        }
+    }
+
+
+   
+}
